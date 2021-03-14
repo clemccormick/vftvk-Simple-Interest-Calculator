6207f3293c1ea5caa96b5de69fc6a8865fb12c3e
Constance McCormick <constance.mccormick@occamsolutions.com>
2021-03-14 13:45:33 -0400

    added html functionality, css, and js variables

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+
 }
-        
+principal = "principal"
+rate = "rate"
+years = "years"
+interest = principal * years * rate / 100
+no_of_years = 2021 + years
