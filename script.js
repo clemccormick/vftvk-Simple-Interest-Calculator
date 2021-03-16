0bcd8a50e0a158017e2e050ebe2a9d5310d92452
clemccormick <48796757+clemccormick@users.noreply.github.com>
2021-03-16 08:47:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ function compute() {
   var n = new Date();
   var y = n.getFullYear() + years
   var interest = principal * years * rate / 100;
-  var text = '<br> If you deposit <mark>' + principal + '</mark>, </br> at an interest rate of <mark>' + rate + '</mark>. </br> You  will receive an amount of <mark>' + interest + '</mark>, </br>in the year <mark>' + y + '</mark>.'
+  
+  if (principal <= 0) {
+  	alert("Please enter a positive number")
+  }
+  else if (isNaN(parseFloat(principal))) {
+  	alert("Please enter a positive number")
+  }
+  else {
+    var text = '<br> If you deposit <mark>' + principal + '</mark>, </br> at an interest rate of <mark>' + rate + '</mark>. </br> You  will receive an amount of <mark>' + interest + '</mark>, </br>in the year <mark>' + y + '</mark>.'
 
-  document.getElementById("result").innerHTML = text
+    document.getElementById("result").innerHTML = text}
 }
 
 function getrange(newvalue){
